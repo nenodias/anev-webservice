75081c41acb54a0d9b36266d882170c36e66a91a
Horácio Dias <horacio.baptista@supportweb.com>
2016-05-04 11:16:50 -0300

    Adding headers optionals

--- server.py
+++ server.py
@@ -18,6 +18,9 @@ def index():
 
 @route('/alunos',['GET'])
 def alunos():
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
     accept = request.GET.get('format') or request.headers.get('Accept')
     query = session.query(Aluno)
     if request.GET.get('aluno'):
