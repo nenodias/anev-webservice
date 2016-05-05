ff28a847f6bc8a9673bf878e30301f1c89f8a592
Horácio Dias <horacio.baptista@supportweb.com>
2016-05-05 14:03:10 -0300

    Removendo xml 2 json e fazendo conversão manual utilizando o $.parseXML

--- static/js/home_controller.js
+++ static/js/home_controller.js
@@ -1,7 +1,7 @@
 (function() {
     'use strict';
     angular.module('app')
-    .controller('HomeController', ['$scope', '$http', 'ngXml2json', function($scope, $http, ngXml2json) {
+    .controller('HomeController', ['$scope', '$http', function($scope, $http) {
         $scope.aluno = null;
         $scope.nome =  null;
         var JSON = { id: 1, name: 'JSON' };
@@ -31,11 +31,25 @@
    			.then(function(response){
    				var retorno = response.data;
           $scope.data = retorno;
+
+          //Tratando Hierarquia do xml
           if(format === 'xml'){
-            retorno = ngXml2json.xml_to_object(retorno);
+            var $xmlObject = $.parseXML( retorno );
+            var alunos = [];
+            $xmlObject = $($xmlObject);
+            $xmlObject.children().children().each(function(){
+              var aluno = {};
+              aluno['aluno'] = new Number( $(this).find('aluno').text() );
+              aluno['nome'] = $(this).find('nome').text();
+              aluno['cpf'] = $(this).find('cpf').text();
+              aluno['rg'] = $(this).find('rg').text();
+              aluno['data_nasc'] = $(this).find('data_nasc').text();
+              alunos.push(aluno);
+            });
+            retorno = {"alunos": alunos};
           }
    				$scope.registros = retorno.alunos;
-   				console.log(retorno.alunos)
+   				console.log(retorno.alunos);
    			});
    		};
     }])
